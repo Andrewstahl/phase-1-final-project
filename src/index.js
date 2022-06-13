8bbbc770887162c8ecaab22c779286a6ac6d54dc
Andrew Stahl <andrewstahl96@gmail.com>
2022-06-12 20:36:19 -0400

    Added initial structure and notes to functions

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// save for grabbing the holiday names - "https://date.nager.at/api/v2/publicholidays/2022/US"
+/**
+ * Basic setup:
+ * - Upon loading, we need to populate the two dropdowns with years and holiday names
+ *    - The holidays will be populated through the API but we can utilize a JSON server for this
+ * - Event listeners:
+ *    - Submit button to populate dates
+ *    - Click button to calculate the days until we hit that holiday
+ * - Separate functions
+ *    - 
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+  initialize();
+})
+
+function initialize() {
+
+}
